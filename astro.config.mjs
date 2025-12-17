a989e7ac082bcc96d03330124da75440973e1868
Pablo Cesar Velazquez Cordova <pablo.velazquez@regishrgroup.com>
2025-12-17 08:31:56 -0600

    update site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://example.com",
+  site: "https://cesar-blog-feed.netlify.app",
   integrations: [mdx()],
 });
